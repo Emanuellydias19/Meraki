54fe97554e1f22bce1cdecf99e0012524fb510ab
Nicole Zanin <nicole.silva@sou.inteli.edu.br>
2025-10-27 15:40:41 -0300

    tests (first part)

--- migrations/deploy.js
+++ migrations/deploy.js
@@ -2,9 +2,6 @@ const anchor = require("@coral-xyz/anchor");
 
 module.exports = async function (provider) {
   anchor.setProvider(provider);
-
   const program = anchor.workspace.MerakiContract;
-
-  console.log("Deploying Meraki Contract...");
-  console.log("Program ID:", program.programId.toString());
+  console.log("Deploying Meraki Contract:", program.programId.toString());
 };
